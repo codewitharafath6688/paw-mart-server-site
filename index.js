9f0ea8056d52434991f949b579d42ccf549d0086
FiaruProject <111216829+FiaruProject@users.noreply.github.com>
2025-11-13 20:28:36 +0600

    allpets & addList added

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const express = require("express");
 const app = express();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+require("dotenv").config()
+const { MongoClient, ServerApiVersion } = require("mongodb");
 const cors = require("cors");
 const port = process.env.PORT || 3000;
-
 
 app.use(cors());
 app.use(express.json());
@@ -15,15 +15,48 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
-app.get('/', (req, res) => {
-    res.send("We are in Back-End")
-})
+app.get("/", (req, res) => {
+  res.send("We are in Back-End");
+});
 
+async function run() {
+  try {
+    await client.connect();
+    const db = client.db("paw-mart-user");
+    const petCollection = db.collection("pets");
+    const addCollection = db.collection("addList")
 
+    app.get('/pets', async (req, res) => {
+        const cursor = petCollection.find().limit(6);
+        const result = await cursor.toArray();
+        res.send(result);
+    })
+
+    app.get('/allpets', async (req, res) => {
+        const cursor = petCollection.find();
+        const result = await cursor.toArray();
+        res.send(result);
+    })
+
+    app.post('/addList', async (req, res) => {
+        const newAdd = req.body;
+        const result = await addCollection.insertOne(newAdd);
+        res.send(result);
+    })
+
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    
+  }
+}
+run().catch(console.dir);
 
 app.listen(port, () => {
-    console.log("Back-End is active");
-})
+  console.log("Back-End is active");
+});
