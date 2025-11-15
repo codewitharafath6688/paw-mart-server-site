ecbdd50873a0c5a21e4ef46bd4d4a005b1cd714e
FiaruProject <111216829+FiaruProject@users.noreply.github.com>
2025-11-15 07:04:55 +0600

    some changes in set up

--- index.js
+++ index.js
@@ -50,7 +50,7 @@ async function run() {
 
     app.post("/addList", async (req, res) => {
       const newItem = req.body;
-      const result =  addCollection.insertOne(newItem);
+      const result = await addCollection.insertOne(newItem);
       res.send(result);
     });
 
